e091d68959e56eba187fc5c4fc75acd5fa02fdf6
Hamaryuginh <lordzabortion666@gmail.com>
2015-10-23 22:50:02 +0200

    Add metrics Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,136 @@
 # GoogleAnalyticsBundle
 
 Work in progress
+
+
+# Installation
+
+Install [composer](https://getcomposer.org/)
+
+Add the following to your require block in composer.json config
+
+```
+"hamaryuginh/google-analytics-bundle": "dev-master"
+```
+
+Then, execute the following command in your symfony poject root
+
+```
+$ composer install
+```
+
+
+# Configuration
+
+Register the bundle in `app/AppKernel.php`:
+
+```php
+public function registerBundles()
+{
+    ...
+    new Hamaryuginh\GoogleAnalyticsBundle\HamaryuginhGoogleAnalyticsBundle(),
+    ...
+}
+```
+
+Add to your `app/config/config.yml` the following:
+
+```yaml
+hamaryuginh_google_analytics:
+    account: UA-XXXXXX-Y   # Your GA account ID
+    debug:   false         # [Facultative] set to true to activate debug mode
+```
+
+Add to your `app/config/routing.yml` the following:
+
+```yaml
+hamaryuginh_google_analytics:
+    resource: "@HamaryuginhGoogleAnalyticsBundle/Resources/config/routing.xml"
+    prefix:   /
+```
+
+Add the following before `</head>` tag:
+
+```twig
+{{ render(controller('HamaryuginhGoogleAnalyticsBundle:GA:render')) }}
+```
+
+At the end of your document, before the `</body>` tag, add:
+
+```html
+<script type="text/javascript">
+    gat.go();
+</script>
+```
+
+
+# Usage
+
+Globally, you define a `data-ga-xxx` attribute on any tag and you give arguments as JSON string (ex: `data-ga-page="{'page':'/home'}"`)
+
+__Page tracking__
+
+Add `data-ga-page` attribute on `<body>` tag or any other tag:
+
+```html
+<body data-ga-page="{'page':'/home'}">
+  ...
+</body>
+```
+
+Or:
+
+```html
+<body>
+  ...
+  <div data-ga-page="{'page':'/home'}"></div>
+  ...
+</body>
+```
+
+Or whatever you want...
+
+> **Warning!** `data-ga-page` must be defined only **once** per document !
+
+For further information, take a look at the [Page tracking](https://developers.google.com/analytics/devguides/collection/analyticsjs/pages) documentation.
+
+__Custom dimentions and metrics__
+
+Add `data-ga-dimension` attribute on any tag:
+
+```html
+<body>
+  ...
+  <div data-ga-dimension="{'index':1, 'value':'Hello'}"></div>
+  <div data-ga-dimension="{'index':2, 'value':'World!!!'}"></div>
+  ...
+  <div data-ga-metric="{'index':18, 'value':8000}"></div>
+  <div data-ga-metric="{'index':19, 'value':24.99}"></div>
+  ...
+</body>
+```
+
+For further information, take a look at the [Custom dimensions and metrics](https://developers.google.com/analytics/devguides/collection/analyticsjs/custom-dims-mets) documentation.
+
+__Event tracking (only "click" event at the moment)__
+
+Add `data-ga-event` attribute on any tag:
+
+```html
+<body>
+  ...
+  <a href="#" data-ga-event="{'eventCategory':'Link','eventAction':'click','eventLabel':'link 1'}">Home</a>
+  ...
+  <div data-ga-event="{'eventCategory':'Element','eventAction':'click','eventLabel':'On div'}"></div>
+  ...
+</body>
+```
+
+For further information, take a look at the [Event tracking](https://developers.google.com/analytics/devguides/collection/analyticsjs/events) documentation.
+
+__Have fun!__
+
+
+# Contributions
+
+Feel free to open an issue or add a pull request.
--- Resources/public/js/tracking.js
+++ Resources/public/js/tracking.js
@@ -1,7 +1,20 @@
 var gat = gat||{};
 
+gat.strings={
+  _def_account: '=> You must define "account" parameter',
+  _def_ga_page: '=> attribute "data-ga-page" must be defined once per document',
+  _def_ga_page_attr: '=> You must define "page" attribute',
+  _def_ga_index_attr: '=> You must define "index" attribute',
+  _def_ga_value_attr: '=> You must define "value" attribute',
+  _def_ga_value_nan: '=> You must provide a number for "value" attribute',
+  _def_ga_event_category_attr: '=> You must define "eventCategory" attribute',
+  _def_ga_event_action_attr: '=> You must define "eventAction" attribute'
+};
+
 gat.init=function(account, debug){
-  if(undefined==account){throw "argument 'account' must be defined"}
+  if(undefined==account){
+    throw this.strings._def_account
+  }
   debug=(debug==undefined?'':'_debug');
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -11,33 +24,78 @@ gat.init=function(account, debug){
 };
 
 gat.go=function(){
-  // handle data-ga-xxx
+  // handle data-ga-xxx attributes
   (function(f,a,b,i,e,n){
+    var na,nb,nc;
     // Page
     n=a.querySelectorAll('[data-ga-page]');
-    if (n.length>1){throw "attribute 'data-ga-page' must be defined once per document"}
-    n=n[0];var d=JSON.parse(n.getAttribute('data-ga-page').replace(/'/g,'"'));
-    if (undefined==d['page']){throw "You must define 'page' attribute"}
-    ga('set',d);
+    if (n.length>1) {
+      //throw self.strings._def_ga_page
+      throw b.strings._def_ga_page
+    }
+    n=n[0];
+    na=JSON.parse(n.getAttribute('data-ga-page').replace(/'/g,'"'));
+    if (undefined==na['page']) {
+      throw b.strings._def_ga_page_attr
+    }
+    ga('set',na);
+
     // Dimensions
     n=a.querySelectorAll('[data-ga-dimension]');
-    for(i=0;i< n.length;i++){
-      var d=JSON.parse(n[i].getAttribute('data-ga-dimension').replace(/'/g,'"'));
-      if (undefined==d['index']){throw "You must define 'index' attribute"}
-      if (undefined==d['value']){throw "You must define 'value' attribute"}
-      d['dimension'+d['index']]=d['value'];delete d['index'];delete d['value'];
-      ga('set',d);
+    for(i=0;i< n.length;i++) {
+      nb=JSON.parse(n[i].getAttribute('data-ga-dimension').replace(/'/g,'"'));
+      if (undefined==nb['index']) {
+        throw b.strings._def_ga_index_attr
+      }
+      if (undefined==nb['value']) {
+        throw b.strings._def_ga_value_attr
+      }
+      nb['dimension'+nb['index']]=nb['value'];
+      delete nb['index'];
+      delete nb['value'];
+      ga('set',nb);
     }
+
+    // Metrics
+    n=a.querySelectorAll('[data-ga-metric]');
+    for(i=0;i< n.length;i++) {
+      nb=JSON.parse(n[i].getAttribute('data-ga-metric').replace(/'/g,'"'));
+      if (undefined==nb['index']) {
+        throw b.strings._def_ga_index_attr
+      }
+      if (undefined==nb['value']) {
+        throw b.strings._def_ga_value_attr
+      }
+      if (isNaN(parseFloat(nb['value'])) || !isFinite(nb['value'])) {
+        throw b.strings._def_ga_value_nan
+      }
+      nb['metric'+nb['index']]=nb['value'];
+      delete nb['index'];
+      delete nb['value'];
+      ga('set',nb);
+    }
+
     // Events
     n=a.querySelectorAll('[data-ga-event]');
-    for(i=0;i< n.length;i++){
-      var d=JSON.parse(n[i].getAttribute('data-ga-event').replace(/'/g,'"'));d['hitType']='event';
-      if (undefined==d['eventCategory']){throw "You must define 'eventCategory' attribute"}
-      if (undefined==d['eventAction']){throw "You must define 'eventAction' attribute"}
-      d['hitCallback']=(function(el){f.location=el.href||el.getAttribute('data-url')}).bind(null,n[i]);
-      n[i].addEventListener('click',function(ev){ev.preventDefault();ga('send',d)});
+    for(i=0;i< n.length;i++) {
+      nc=JSON.parse(n[i].getAttribute('data-ga-event').replace(/'/g,'"'));
+      nc['hitType']='event';
+      if (undefined==nc['eventCategory']){
+        throw b.strings._def_ga_event_category_attr
+      }
+      if (undefined==nc['eventAction']){
+        throw b.strings._def_ga_event_action_attr
+      }
+      nc['hitCallback']=(function(el){
+        if (el.href||el.getAttribute('data-url')) {
+          f.location=el.href||el.getAttribute('data-url')
+        }
+      }).bind(null,n[i]);
+      n[i].addEventListener('click',(function(inc){
+        return function(ev){ev.preventDefault();ga('send',inc)}
+      })(nc));
     }
-  })(window, document);
+  })(window, document, this);
 
-  ga('send', 'pageview');/*Must be launch at the end of the document, just before </body>*/
+  ga('send', 'pageview');
 };
