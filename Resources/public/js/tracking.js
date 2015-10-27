46744ab1329823f97ea07f818ef7a23c021fb21d
Hamaryuginh <lordzabortion666@gmail.com>
2015-10-28 00:21:32 +0100

    Refactor tracking.js

--- Resources/public/js/tracking.js
+++ Resources/public/js/tracking.js
@@ -3,15 +3,15 @@ var gat = gat||{};
 gat.strings={
   _def_account: '=> You must define "account" parameter',
   _def_ga_page: '=> attribute "data-ga-page" must be defined once per document',
-  _def_ga_page_attr: '=> You must define "page" attribute',
-  _def_ga_index_attr: '=> You must define "index" attribute',
-  _def_ga_value_attr: '=> You must define "value" attribute',
-  _def_ga_value_nan: '=> You must provide a number for "value" attribute',
-  _def_ga_event_category_attr: '=> You must define "eventCategory" attribute',
-  _def_ga_event_action_attr: '=> You must define "eventAction" attribute',
-  _def_ga_social_network_attr: '=> You must define "socialNetwork" attribute',
-  _def_ga_social_action_attr: '=> You must define "socialAction" attribute',
-  _def_ga_social_target_attr: '=> You must define "socialTarget" attribute'
+  _def_attr_tmpl: '=> You must define "%attr%" attribute for data-ga-%prop% property',
+  _def_type_tmpl: '=> Attribute "%attr%" must be of type "%type%"'
+};
+
+gat.attrError=function(prop, attr){
+  throw this.strings._def_attr_tmpl.replace('%prop%', prop).replace('%attr%', attr);
+};
+gat.typeError=function(type, attr){
+  throw this.strings._def_type_tmpl.replace('%type%', type).replace('%attr%', attr);
 };
 
 gat.init=function(account, debug){
@@ -30,100 +30,108 @@ gat.go=function(){
   // handle data-ga-xxx attributes
   (function(f,a,b,i,e,n){
     var na,nb,nc,nd;
-    // Page
-    n=a.querySelectorAll('[data-ga-page]');
-    if (n.length>1) {
-      //throw self.strings._def_ga_page
-      throw b.strings._def_ga_page
-    }
-    n=n[0];
-    na=JSON.parse(n.getAttribute('data-ga-page').replace(/'/g,'"'));
-    if (undefined==na['page']) {
-      throw b.strings._def_ga_page_attr
-    }
-    ga('set',na);
-
-    // Dimensions
-    n=a.querySelectorAll('[data-ga-dimension]');
-    for(i=0;i< n.length;i++) {
-      nb=JSON.parse(n[i].getAttribute('data-ga-dimension').replace(/'/g,'"'));
-      if (undefined==nb['index']) {
-        throw b.strings._def_ga_index_attr
-      }
-      if (undefined==nb['value']) {
-        throw b.strings._def_ga_value_attr
-      }
-      nb['dimension'+nb['index']]=nb['value'];
-      delete nb['index'];
-      delete nb['value'];
-      ga('set',nb);
-    }
-
-    // Metrics
-    n=a.querySelectorAll('[data-ga-metric]');
-    for(i=0;i< n.length;i++) {
-      nb=JSON.parse(n[i].getAttribute('data-ga-metric').replace(/'/g,'"'));
-      if (undefined==nb['index']) {
-        throw b.strings._def_ga_index_attr
-      }
-      if (undefined==nb['value']) {
-        throw b.strings._def_ga_value_attr
-      }
-      if (isNaN(parseFloat(nb['value'])) || !isFinite(nb['value'])) {
-        throw b.strings._def_ga_value_nan
-      }
-      nb['metric'+nb['index']]=nb['value'];
-      delete nb['index'];
-      delete nb['value'];
-      ga('set',nb);
-    }
-
-    // Events
-    n=a.querySelectorAll('[data-ga-event]');
-    for(i=0;i<n.length;i++) {
-      nc=JSON.parse(n[i].getAttribute('data-ga-event').replace(/'/g,'"'));
-      nc['hitType']='event';
-      if (undefined==nc['eventCategory']){
-        throw b.strings._def_ga_event_category_attr
-      }
-      if (undefined==nc['eventAction']){
-        throw b.strings._def_ga_event_action_attr
-      }
-      nc['hitCallback']=(function(el){
-        if (el.href||el.getAttribute('data-url')) {
-          f.location=el.href||el.getAttribute('data-url')
+    // Shim
+    if (!Date.now){Date.now = function() { return new Date().getTime(); }}
+    // Generic parser
+    var __l = {
+      page:{
+        required: ['page'],
+        optional: ['location','title'],
+        init: function(el){
+          el.ga_data.hitType = 'page';
+          ga('set', el.ga_data)
         }
-      }).bind(null,n[i]);
-      n[i].addEventListener('click',(function(inc){
-        return function(ev){ev.preventDefault();ga('send',inc)}
-      })(nc));
-    }
-
-    // Social (for homemade social buttons)
-    n=a.querySelectorAll('[data-ga-social]');
-    for(i=0;i<n.length;i++) {
-      nd=JSON.parse(n[i].getAttribute('data-ga-social').replace(/'/g,'"'));
-      nd['hitType']='social';
-      if (undefined==nd['socialNetwork']){
-        throw b.strings._def_ga_social_network_attr
-      }
-      if (undefined==nd['socialAction']){
-        throw b.strings._def_ga_social_action_attr
-      }
-      if (undefined==nd['socialTarget']){
-        throw b.strings._def_ga_social_target_attr
-      }
-      n[i].addEventListener('click',(function(inc){
-        return function(ev){
-          var _this = this;
-          if(!_this.hasAttribute('ga-social-done')){
-            ev.preventDefault();
-            ga('send',inc);
-            _this.setAttribute('ga-social-done','1');
-            setTimeout(function(){_this.click();_this.removeAttribute('ga-social-done')},100);
+      },
+      dimension:{
+        required: ['index','value'],
+        optional: [],
+        init: function(el){
+          el.ga_data['dimension'+el.ga_data.index] = el.ga_data.value;
+          delete el.ga_data.index;
+          delete el.ga_data.value;
+          ga('set',el.ga_data)
+        }
+      },
+      metric:{
+        required: ['index','value'],
+        optional: [],
+        init: function(el){
+          if (isNaN(parseFloat(el.ga_data.value)) || !isFinite(el.ga_data.value)) {
+            b.typeError('Number', 'value')
           }
+          el.ga_data['metric'+el.ga_data.index] = el.ga_data.value;
+          delete el.ga_data.index;
+          delete el.ga_data.value;
+          ga('set',el.ga_data)
         }
-      })(nd));
+      },
+      event:{
+        required: ['eventCategory','eventAction'],
+        optional: ['eventLabel','eventValue'],
+        init: function(el){
+          el.ga_data.hitType = 'event';
+          el.ga_data.hitCallback = (function(el){
+            if (el.href||el.getAttribute('data-url')) {
+              f.location=el.href||el.getAttribute('data-url')
+            }
+          }).bind(null,el);
+          el.addEventListener('click',(function(gad){
+            return function(e){e.preventDefault();ga('send',gad)}
+          })(el.ga_data))
+        }
+      },
+      social:{
+        required: ['socialNetwork','socialAction','socialTarget'],
+        optional: [],
+        init: function(el){
+          el.ga_data.hitType = 'social';
+          el.addEventListener('click',(function(gad){
+            return function(e){
+              var _this = this;
+              if(!_this.hasAttribute('ga-social-done')){
+                e.preventDefault();
+                ga('send',gad);
+                _this.setAttribute('ga-social-done','1');
+                setTimeout(function(){_this.click();_this.removeAttribute('ga-social-done')},100);
+              }
+            }
+          })(el.ga_data))
+        }
+      },
+      timing:{
+        required: ['timingCategory','timingVar'], // required attribute "timingValue" is calculated
+        optional: ['timingLabel'],
+        init: function(el){
+          el.ga_data.hitType = 'timing';
+          el.start = function() {
+            this.timing_start = Date.now()
+          };
+          el.stop = function() {
+            if (!this.timing_start) return;
+            this.ga_data['timingValue'] = Date.now() - this.timing_start;
+            ga('send',this.ga_data)
+          };
+        }
+      }
+    };
+    for (var key in __l) {
+      if (!__l.hasOwnProperty(key)) return;
+      var __ld = __l[key];
+      var __n = a.querySelectorAll('[data-ga-'+key+']');
+      for (var j=0; j<__n.length; j++) {
+        (function(el,dat,prop) {
+          // Get ga-data
+          el.ga_data = JSON.parse(el.getAttribute('data-ga-'+key).replace(/'/g,'"'));
+          // Check parameters
+          dat['required'].forEach(function(attr){
+            if (undefined==el.ga_data[attr]) {
+              b.attrError(prop, attr)
+            }
+          });
+          // Init
+          dat.init(el)
+        })(__n[j],__ld,key)
+      }
     }
   })(window, document, this);
 
